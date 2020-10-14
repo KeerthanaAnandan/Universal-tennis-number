d80cab79f7394f7b603ab7d4543233c07901ac85
keerthanaAnandan <keerthanaanandanak@gmail.com>
2020-10-14 14:22:50 +0530

    updated

--- dist/js/start.js
+++ dist/js/start.js
@@ -1,47 +1,36 @@
 function submitForm(e) {
   e.preventDefault();
   let name = document.forms["welcome_form"]["name"].value;
-  //  let user_age = document.querySelector(".welcome_form.active").innerHTML;
 
   sessionStorage.setItem("points", points);
-  //console.log(user_age);
 
   location.href = "quiz.html";
 }
-// AOS Instance
-//AOS.init();
+
 let points = 0;
 function onepoint() {
   points += -90;
-  console.log(points + "firstpoint");
 }
 function twopoint() {
   points += -30;
-  console.log(points + "secpoint");
 }
 function threepoint() {
   points += 12;
-  console.log(points + "3point");
 }
 function fourpoint() {
   points += 30;
-  console.log(points + "4point");
 }
 function fivepoint() {
   points += 48;
-  console.log(points + "5point");
 }
 function sixpoint() {
   points += 40;
-  console.log(points + "6point");
 }
 function sevenpoint() {
   points += 15;
-  console.log(points + "7oint");
 }
 function eightpoint() {
   points += 9;
-  console.log(points + "8oint");
 }
 
 sessionStorage.setItem("points", points);
--- dist/js/userInfo.js
+++ dist/js/userInfo.js
@@ -1,49 +1,35 @@
-//let user_name = sessionStorage.getItem("name");
 let user_points = sessionStorage.getItem("points");
-//let user_time = sessionStorage.getItem("time");
-//document.querySelector("span.name").innerHTML = user_name;
 
-//document.querySelector("span.time_taken").innerHTML = user_time;
 let pointop = 0;
 if (user_points >= 287 && user_points <= 318) {
-  console.log("1");
   pointop = 1;
 }
 if (user_points >= 255 && user_points <= 286) {
-  console.log("2");
   pointop = 2;
 }
 if (user_points >= 223 && user_points <= 254) {
-  console.log("3");
   pointop = 3;
 }
 if (user_points >= 191 && user_points <= 222) {
-  console.log("4");
   pointop = 4;
 }
 if (user_points >= 159 && user_points <= 190) {
-  console.log("5");
   pointop = 5;
 }
 if (user_points >= 127 && user_points <= 158) {
-  console.log("6");
   pointop = 6;
 }
 if (user_points >= 95 && user_points <= 126) {
-  console.log("7");
   pointop = 7;
 }
 if (user_points >= 63 && user_points <= 94) {
-  console.log("8");
   pointop = 8;
 }
 if (user_points >= 31 && user_points <= 62) {
-  console.log("9");
   pointop = 9;
 }
 if (user_points >= -51 && user_points <= 30) {
   pointop = 10;
-  console.log("10");
 }
 document.querySelector("span.points").innerHTML = pointop;
 if (pointop == 1) {
